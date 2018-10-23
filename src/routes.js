b5b825012b9ba40ab34d3483d8bf7eb1b54cb4ab
Oleksandr Makhynenko <oleksandr.makhynenko@digitallyinspired.com>
2018-10-23 07:39:32 +0300

    feaature/redux

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import {
     BrowserRouter as Router,
-    Route,
+    Route, Switch, Redirect,
 } from 'react-router-dom';
 
-import Home from './views/home';
+import Home from './views/home/Home';
+import List from './views/list/List';
 
 export default () => (
     <Router>
-        <Route path="/" component={Home} />
+        <Switch>
+            <Route path="/list" component={List} />
+            <Route path="/home" component={Home} />
+            <Redirect to="/home" />
+        </Switch>
     </Router>
 );
